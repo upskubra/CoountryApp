c9b8c4192b5c39aead8719e367f055b482882511
upskubra <yildirimk268@gmail.com>
2021-05-29 20:33:39 +0300

    model data class

---
+++ app/src/main/java/com/example/countryapp/model/Country.kt
@@ -0,0 +1,14 @@
+package com.example.countryapp.model
+
+
+data class Country(
+    val countryName: String?,
+    val countryCapital: String?,
+    val countryRegion: String?,
+    val countryCurrency: String?,
+    val countryLanguage : String?,
+    val countryImageUrl : String?
+
+) {
+
+}
