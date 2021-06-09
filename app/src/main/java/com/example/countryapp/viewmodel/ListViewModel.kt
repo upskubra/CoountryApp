d689810d1f799c5578fb0a9554433848b8019c44
upskubra <yildirimk268@gmail.com>
2021-06-09 21:52:31 +0300

    get data in ViewModel

--- app/src/main/java/com/example/countryapp/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/countryapp/viewmodel/ListViewModel.kt
@@ -3,23 +3,60 @@ package com.example.countryapp.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.countryapp.model.Country
+import com.example.countryapp.service.CountryAPIService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class ListViewModel : ViewModel() {
+
+    private val countryApiService = CountryAPIService()
+    // use and at object, required for memory management
+    private val disposable = CompositeDisposable()
+
+
     val countries = MutableLiveData<List<Country>>()
     val countryErrorMessage = MutableLiveData<Boolean>()
     val countryProgressBar = MutableLiveData<Boolean>()
 
     fun refreshData() {
 
-        // example data
-        val turkey = Country("Turkey", "Ankara", "Asia", "TRY", "Turkish", "www.test.com")
-        val turkey1 = Country("Turkey1", "Ankara1", "Asia1", "TRY1", "Turkish1", "www.test.com")
-        val turkey2 = Country("Turkey2", "Ankara2", "Asia2", "TRY2", "Turkish2", "www.test.com")
+        getDataFromAPI()
 
-        val countryList = arrayListOf<Country>(turkey, turkey1, turkey2)
-        countries.value = countryList
-        countryErrorMessage.value = false
-        countryProgressBar.value = false
+    }
+
+    fun getDataFromAPI() {
+
+        countryProgressBar.value = true
+
+        disposable.add(
+            countryApiService.getData()
+                    // different thread because asynchronous, working background
+                .subscribeOn(Schedulers.newThread())
+
+                .observeOn(AndroidSchedulers.mainThread())
+
+                .subscribeWith(object : DisposableSingleObserver<List<Country>>(){
+                    override fun onSuccess(t: List<Country>) {
+
+                        countries.value = t
+                        countryErrorMessage.value = false
+                        countryProgressBar.value = false
+
+                    }
+
+                    override fun onError(e: Throwable) {
+                        countryErrorMessage.value = true
+                        countryProgressBar.value = false
+                        e.printStackTrace()
+
+                    }
+
+                })
+        )
+
+
 
     }
 
